6adeefe9a7af0ed84290d1adfa3cfb2ee4ad20e7
Your NameSergey <mserg-90@mail.ru>
2022-02-12 18:03:22 +0300

    Настройка Дженкинса 1

--- src/main/java/telegram/Telegram.java
+++ src/main/java/telegram/Telegram.java
@@ -10,14 +10,36 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Telegram extends TelegramLongPollingBot {
 
+    private String botUsername;
+    private String botToken;
 
+    public Telegram () {
+        super();
+
+        //** Получаю данные из конфига **
+        File file = new File("./config.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        botUsername = properties.getProperty("botName");
+        botToken = properties.getProperty("botToken");
+    }
 
     SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
     SendPhoto sendPhoto = new SendPhoto();
 
+
+    //    ** Метод для отправки сообщения в телеграм **
     public void sendNewMessage(String newMessage, Update update) {
 
         message.setChatId(update.getMessage().getChatId().toString());
@@ -30,6 +52,7 @@ public class Telegram extends TelegramLongPollingBot {
         }
     }
 
+    //    ** Метод для отправки скриншота в телеграм **
     public String sendNewPhoto(String fileLocation, Update update) {
 
         try {
@@ -60,13 +83,17 @@ public class Telegram extends TelegramLongPollingBot {
             //** Разбор входящего запроса из телеги на элементы **
             String[] inputQuery = update.getMessage().getText().split(" ");
 
-            if (inputQuery.length < 1){
+            if (inputQuery.length < 1) {
                 id_test = null;
-            } else {id_test = inputQuery[0];}
+            } else {
+                id_test = inputQuery[0];
+            }
 
-            if (inputQuery.length < 2){
+            if (inputQuery.length < 2) {
                 id_documents = null;
-            } else {id_documents = inputQuery[1];}
+            } else {
+                id_documents = inputQuery[1];
+            }
 
             switch (id_test) {
                 case "1": {
@@ -79,9 +106,9 @@ public class Telegram extends TelegramLongPollingBot {
                     break;
                 }
                 case "2": {
-                    if(id_documents == null){
-                        sendNewMessage("Некорректный ID документа", update);}
-                    else {
+                    if (id_documents == null) {
+                        sendNewMessage("Некорректный ID документа", update);
+                    } else {
                         Test2 test2 = new Test2();
                         sendNewMessage("Старт теста №2", update);
                         test2.startTest(file -> sendNewPhoto(file, update), inputQuery[1]);
@@ -104,11 +131,11 @@ public class Telegram extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "PVSO_auto_tests_bot";
+        return botUsername;
     }
 
     @Override
     public String getBotToken() {
-        return "5200651683:AAFj7z5VFvaFj-Dzt5h-ci3LpkYEqQXjgVk";
+        return botToken;
     }
 }
